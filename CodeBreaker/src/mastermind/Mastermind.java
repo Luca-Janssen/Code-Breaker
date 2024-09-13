6a4fd958f10048ee3362b3072c85561bb0ab2363
LJW <1210790@student.roc-nijmegen.nl>
2024-09-13 15:56:47 +0200

    update

---
+++ CodeBreaker/src/mastermind/Mastermind.java
@@ -0,0 +1,16 @@
+package mastermind;
+
+public class Mastermind {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String blauw = "bl";
+String rood = "rd";
+String yellow = "yl";
+String Groen = "gr";
+String Paars = "pr";
+String Wit = "wt";
+String Zwart = "zw";
+	}
+
+}
