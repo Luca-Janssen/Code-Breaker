77249533fd441bf7f77cc52ef488ac6fbba3692e
LJW <1210790@student.roc-nijmegen.nl>
2025-01-27 15:26:45 +0100

    Algemene verbetering code
    De code is algemeen verbeterd met wat quality-of-life changes en alles is omgezet naar functions

--- CodeBreaker/src/mastermind/Mastermind.java
+++ CodeBreaker/src/mastermind/Mastermind.java
@@ -1,98 +1,37 @@
 package mastermind;
 
-import java.util.*;
-
 public class Mastermind {
-    private static Scanner input = new Scanner(System.in);
-    private static Random random = new Random();
-
     public static void main(String[] args) {
-        String[] c = new String[8];
-
-        c[0] = "Black";  
-        c[1] = "White";   
-        c[2] = "Yellow"; 
-        c[3] = "Red";    
-        c[4] = "Blue";    
-        c[5] = "Green";   
-        c[6] = "Purple";  
-        c[7] = "Orange"; 
-       
-
-        String[] codeArray = new String[4];
-        codeArray[0] = getRandomColor(c);
-        codeArray[1] = getRandomColor(c);
-        codeArray[2] = getRandomColor(c);
-        codeArray[3] = getRandomColor(c);
-
-        int colorsright = 0;
-        int rowloop;
-
-        System.out.println("Guess the code using the following colors: Yellow, Red, Blue, Green, Purple, and Orange. You get 10 attempts before you lose. The colour input is case-sensitive so start with capital letters.");
-        for (rowloop = 1; rowloop <= 10; rowloop++) {
-        	  String[] userColor = new String[4];
-
-            System.out.println("Color one");
-            userColor[0] = input.nextLine();
-            System.out.println("Color two");
-            userColor[1] = input.nextLine();
-            System.out.println("Color three");
-            userColor[2] = input.nextLine();
-            System.out.println("Color four");
-            userColor[3] = input.nextLine();
-
-            colorsright = 0;
-
-            if (userColor[0].equals(codeArray[0] )) {
-                System.out.println("black");
-                colorsright++;
-            } else if (userColor[1].equals(codeArray[0]) || userColor[0].equals(codeArray[2]) || userColor[0].equals(codeArray[3])) {
-                System.out.println("white");
-            } else {
-                System.out.println("false");
-            }
-
-            if (userColor[1].equals(codeArray[1])) {
-                System.out.println("black");
-                colorsright++;
-            } else if (userColor[1].equals(codeArray[0] ) || userColor[1].equals(codeArray[2]) || userColor[1].equals(codeArray[3])) {
-                System.out.println("white");
-            } else {
-                System.out.println("false");
-            }
-
-            if (userColor[2].equals(codeArray[2])) {
-                System.out.println("black");
-                colorsright++;
-            } else if (userColor[2].equals(codeArray[0] ) || userColor[2].equals(codeArray[1]) || userColor[2].equals(codeArray[3])) {
-                System.out.println("white");
-            } else {
-                System.out.println("false");
-            }
-
-            if (userColor[3].equals(codeArray[3])) {
-                System.out.println("black");
-                colorsright++;
-            } else if (userColor[3].equals(codeArray[0] ) || userColor[3].equals(codeArray[1]) || userColor[3].equals(codeArray[2])) {
-                System.out.println("white");
-            } else {
-                System.out.println("false");
-            }
-
-            if (colorsright == 4) {
-                break;
+        Mastermind_functie function = new Mastermind_functie();
+        
+        // Generate the computer's code
+        String computerCode = function.computerColor();
+        
+        System.out.println("Guess the code using the following colors: Yellow, Red, Blue, Green, Purple, and Orange.");
+        System.out.println("You get 10 attempts before you lose.");
+        System.out.println("The colour input is case-sensitive so start with capital letters.");
+        
+        // Main game loop
+        for (int attempt = 1; attempt <= 10; attempt++) {
+            System.out.println("\nAttempt " + attempt + " of 10:");
+            
+            // Get user's guess
+            function.getUserColors();
+            
+            // Check the colors and display result
+            String result = function.checkColors();
+            System.out.println("\nResults:");
+            System.out.println(result);
+            
+            // Check if user won
+            if (result.contains("You win!")) {
+                System.out.println("Congratulations! You solved it in " + attempt + " attempts!");
+                return;
             }
         }
-
-        if (colorsright == 4) {
-            System.out.println("you win!");
-        } else {
-            System.out.println("You lost");
-        }
-    }
-
-    private static String getRandomColor(String[] colors) {
-        int index = random.nextInt(colors.length - 2) + 2; 
-        return colors[index];
+        
+        // If we get here, the user has used all 10 attempts without winning
+        System.out.println("\nGame Over! You've used all 10 attempts.");
+        System.out.println("The correct code was: " + computerCode);
     }
 }
---
+++ CodeBreaker/src/mastermind/Mastermind_functie.java
@@ -0,0 +1,70 @@
+package mastermind;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Mastermind_functie {
+    public String[] c = {"Black", "White", "Yellow", "Red", "Blue", "Green", "Purple", "Orange"};
+    public String[] codeArray = new String[4];
+    private Scanner input = new Scanner(System.in);
+    private Random random = new Random();
+    private String[] userColors = new String[4];
+
+    public String getRandomColor(String[] colors) {
+        int index = random.nextInt(colors.length - 2) + 2;
+        return colors[index];
+    }
+
+    public String computerColor() {
+        String colorString = "";
+        for (int i = 0; i < 4; i++) {
+            codeArray[i] = getRandomColor(c);
+            colorString += codeArray[i] + " ";
+        }
+        return colorString.trim();
+    }
+
+    public String getUserColors() {
+        String userColorString = "";
+        
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Enter color " + (i + 1) + ": ");
+            userColors[i] = input.nextLine();
+            userColorString += userColors[i] + " ";
+        }
+        return userColorString.trim();
+    }
+
+    public String checkColors() {
+        String result = "";
+        int colorsRight = 0;
+
+        for (int i = 0; i < 4; i++) {
+            if (userColors[i].equalsIgnoreCase(codeArray[i])) {
+                result += "Black\n";  // Correct color in correct position
+                colorsRight++;
+            } else if (hasColorInOtherPosition(userColors[i], i)) {
+                result += "White\n";  // Correct color in wrong position
+            } else {
+                result += "None\n";   // Color not in code
+            }
+        }
+
+        if (colorsRight == 4) {
+            result += "You win!";
+        } else {
+            result += "Try again!";
+        }
+
+        return result;
+    }
+
+    public boolean hasColorInOtherPosition(String userColor, int currentPos) {
+        for (int i = 0; i < codeArray.length; i++) {
+            if (i != currentPos && userColor.equalsIgnoreCase(codeArray[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
